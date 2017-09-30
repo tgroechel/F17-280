0cdaad4a5d494f930263be40ed3d06402dc06235
Tom Groechel <tgroeche@umich.edu>
2017-09-30 12:06:55 -0400

    Finished lab3

--- lab03/cstring.cpp
+++ lab03/cstring.cpp
@@ -4,5 +4,23 @@
 using namespace std;
 
 int main(){
+
+	char arr [8] = {'a', 'p','\0', 't', 'a','t','o', '\0'};
+	cout << arr << endl;
+	cout << *arr << endl;
+	cout << &arr[0] << endl;
+	cout << arr + 3 << endl;
+
+	const char * const lmao = "here2";
+	//lmao += 3;
+	cout << lmao << endl;
+
+	string cheese = "here";
+	cout << cheese[3] << endl;
+
+	const char * lol = cheese.c_str();
+	cout << lol << endl;
+
+
 	return 0;
 }
