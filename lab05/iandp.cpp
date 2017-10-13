c203500799ec82f4fdc0af12c1aa50399a805f8f
Tom Groechel <tgroeche@umich.edu>
2017-10-13 13:31:11 -0400

    Added lots O stuff

--- lab05/iandp.cpp
+++ lab05/iandp.cpp
@@ -3,6 +3,31 @@
 
 using namespace std;
 
+class Teacher {
+    string name;
+    int t_hours;
+public:
+    Teacher() : name("default"), t_hours(0) {
+        cout << "Teacher Default Constructor" << endl;
+    }
+
+    Teacher(string _name, int _t_hours) : name(_name), t_hours(_t_hours) {
+        cout << "Teacher Overloaded Constructor" << endl;
+    }
+
+    void change_name(string _name) {
+        name = _name;
+        cout << "Teacher name is now: " << name << endl;
+    }
+
+    void increment_hours(){
+        ++t_hours;
+        cout << "Teacher hours: " << t_hours << endl;
+    }
+
+    virtual void teach() = 0;
+
+};
 
 int main(int argc, char ** argv) {
 
