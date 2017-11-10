64cd2564a36c0819642af9a4935b78b5c06abda4
tgroeche <tgroeche@umich.edu>
2017-11-10 10:22:04 -0500

    First lab on Tuesday example

--- lab07/dynamic.cpp
+++ lab07/dynamic.cpp
@@ -4,7 +4,33 @@
 using namespace std;
 
 
+template <typename potato>
+potato print_arr(potato a, int size){
+	for(int i = 0; i < size; ++i){
+		cout << a[i] << ",";
+	}
+	cout << endl;
+
+	return a;
+}
+
 int main(int argc, char ** argv) {
+	double * arraaaaaaaaaay = new double[3]();
+	double * happy = arraaaaaaaaaay;
+
+	
+	arraaaaaaaaaay[2] = 7;
+	print_arr(arraaaaaaaaaay, 3);
+	cout << happy << endl;
+
+	delete[] arraaaaaaaaaay;
+	arraaaaaaaaaay = nullptr;
+	happy = nullptr;
+
+	double * sad = new double[3]();
+	print_arr(sad, 3);
+	print_arr(happy,3);
+
 
     return 0;
 }
