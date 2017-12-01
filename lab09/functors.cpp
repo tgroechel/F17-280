1ee68bb880dabdadf2157acc4e96e81feac93b1f
Tom Groechel <tgroeche@umich.edu>
2017-12-01 14:03:48 -0500

    Start lab

--- lab09/functors.cpp
+++ lab09/functors.cpp
@@ -4,23 +4,7 @@
 
 using namespace std;
 
-class Is_Potato {
-    string s;
-public:
-    Is_Potato(string s_in) : s(s_in) {}
-    bool operator() (string am_I_potato){
-    	return am_I_potato == s;
-    }
-};
 
 int main() {
-	Is_Potato predd("potato");
-	std::vector<string> v(4, "potato");
-	v[2] = "no potato";
 
-	for(auto it = v.begin(); it != v.end(); ++it){
-		if(!predd(*it)){
-			cout << *it << endl;
-		}
-	}
 }
