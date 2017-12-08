c67a9326006defff2094307a75bb23e3782345f4
Tom Groechel <tgroeche@umich.edu>
2017-12-08 12:43:17 -0500

    example recurssin

--- lab10/recursion.cpp
+++ lab10/recursion.cpp
@@ -6,6 +6,20 @@
 using namespace std;
 
 
+int print_stuff(int num){
+	cout << num << endl;
+	if(num <= 0){
+		return 1;
+	}
+	int hold = print_stuff(num - 1);
+
+	cout << hold << endl;
+
+	return hold + 1;
+}
+
 int main() {
+
+	cout << print_stuff(3) << endl;
 
 }
